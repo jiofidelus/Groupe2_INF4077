2bcf94af2298fb6faf90d153ef6972067fa2afc2
Jordan Nguejip <yvesjordan06@gmail.com>
2020-12-04 12:41:18 +0100

    updated

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
 <script lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
-
 export default defineComponent({
   name: 'App',
   components: {
