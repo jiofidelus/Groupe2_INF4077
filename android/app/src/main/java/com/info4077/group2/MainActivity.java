7d3faf2716c43dafc783340c04a1028c6812d940
Jordan Nguejip <yvesjordan06@gmail.com>
2020-12-09 19:37:55 +0100

    Updated project

---
+++ android/app/src/main/java/com/info4077/group2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.info4077.group2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
